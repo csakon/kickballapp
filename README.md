ed803d898862232430585edf73f68191080fb61f
Chad Sakonchick <chadwicksakonchick@gmail.com>
2013-02-06 23:46:31 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+*Kickball Tracking App*
+
+App to track stats of players on a kickball team and auto-create lineups
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickballapp::Application.config.secret_token = '05bc9a56281772f327fb24f7d55e3f6712245ed86ce842f1cb4f5ed06755f6a169966f8475f6a59ac7cb0bc512e4b42b69c738be1528e27f6b1bacbc9aff341d'
